3e3455c06c282742a0341329543fb97625a1c8d3
ElizabethNa7 <elizabethna777@gmail.com>
2024-07-02 23:09:02 -0700

    SQL section

--- flaskr/db.py
+++ flaskr/db.py
@@ -16,5 +16,17 @@ def get_db():
 
 def close_db(e=None):
     db=g.pop('db', None)
-    if db is not None:
+    if db is not None: # if a connection was created, close it
         db.close()
+
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+    
+@click.command('init-db')
+def init_db_command():
+    """Clear existing data and create new tables"""
+    init_db()
+    click.echo('Database initialized.')
